c23a1e939a28a839c3c8d16879fcff4e89543728
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2019-08-03 21:37:04 +0300

    Use express json parser instead of BodyParser

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const items = require('./routes/api/items');
 
 const app = express();
 
-// Bodyparser Middleware
-app.use(bodyParser.json());
+// JSON Parser Middleware
+app.use(express.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
