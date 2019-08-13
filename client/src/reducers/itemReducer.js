37a2d8dfe534de4df828a2bdd96cfd596167b377
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2019-08-13 19:57:36 +0200

    Full stack shopping list with React and Redux

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,21 +1,41 @@
 // a reducer is where our actual state is going to go
+// CHANGES THE FRONT END
 import uuid from "uuid";
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  ITEMS_LOADING
+} from "../actions/types";
 
 const initialState = {
-  items: [
-    { id: uuid(), name: "Eggs" },
-    { id: uuid(), name: "Milk" },
-    { id: uuid(), name: "Steak" },
-    { id: uuid(), name: "Candy" }
-  ]
+  items: [],
+  loading: false
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
       return {
-        ...state
+        ...state,
+        items: action.payload,
+        loading: false
+      };
+    case DELETE_ITEM:
+      const id = action.payload;
+      return {
+        ...state,
+        items: state.items.filter(item => item._id !== id)
+      };
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items]
+      };
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true
       };
     default:
       return state;
